11768d02c73cb41b5cb69566bf71dd0abc91e818
S. Ajit Kumar <ajitkumar1310@hotmail.com>
2015-11-11 16:41:55 +0530

    Initial commit

---
+++ IntroductionToCSharp/IntroductionToCSharp/DataTypes.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntroductionToCSharp
+{
+    class DataTypes
+    {
+
+        static void Main()
+        {
+            int a = 10;
+            bool b = false;
+            float f = 2.33F;
+            decimal d=5.78M;
+            Console.WriteLine("Size of int "+sizeof(int));
+
+        }
+    }
+}
---
+++ IntroductionToCSharp/IntroductionToCSharp/DifferentMains.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntroductionToCSharp
+{
+    class DifferentMains
+    {
+        #region Different Mains
+        //static void Main()
+        //{
+        //    int a = 10, b = 20;
+        //    Console.WriteLine("Hellow!!! Welcome To CSharp World!!!");
+        //    Console.WriteLine(a + b);
+
+        //}
+        //static int Main()
+        //{
+        //    int a = 10, b = 20;
+        //    Console.WriteLine("Hellow!!! Welcome To CSharp World!!!");
+        //    Console.WriteLine(a + b);
+        //    return 1;
+        //}
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hellow!!! Welcome To CSharp World!!!");
+            Console.WriteLine(args[0] + args[1]);
+            int a = int.Parse(args[0]);
+            int b = int.Parse(args[1]);
+            Console.WriteLine(a+b);
+
+        }
+
+        //static int Main(string[] args)
+        //{
+        //Console.WriteLine("Hellow!!! Welcome To CSharp World!!!");
+        //    Console.WriteLine(args[0] + args[1]);
+        //    int a = int.Parse(args[0]);
+        //int b = int.Parse(args[1]);
+        //Console.WriteLine(a+b);
+        //return 0l
+        //}
+
+        #endregion
+    }
+}
