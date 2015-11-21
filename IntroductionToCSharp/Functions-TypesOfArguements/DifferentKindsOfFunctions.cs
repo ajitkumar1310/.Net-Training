043fc9781a114cd0e317eeef50e102d72ef236c2
ajitkumar1310 <ajitkumar1310@gmail.com>
2015-11-21 09:49:42 +0530

    Functions and Arguements,Overloading

---
+++ IntroductionToCSharp/Functions-TypesOfArguements/DifferentKindsOfFunctions.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//class parent
+//{
+//    public void PrintName()
+//    {
+//         Console.WriteLine("My name is Abhinav ");
+//    }
+//}
+
+namespace Functions_TypesOfArguements
+{
+    class DifferentKindsOfFunctions
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Functions and Arguements");
+
+            ////PrintName();
+            //PrintGenName("Abhinav",20);            
+            //VoidMethodWithParams(10, "Ajit");
+            //int result = Add(10, 20);
+            //VoidMethod();
+            //int resultvar;
+            //resultvar=Multply(10, 20);
+            //resultvar = Multply(10, 20);
+            //Console.WriteLine("Multiplication of two numbers is "+resultvar);
+            //Console.WriteLine("Addition of two numbers is "+result);
+            //int subrest;
+            //subrest = Substrct(100, 50);
+            //Console.WriteLine(subrest);
+            Console.WriteLine("Add method with one param");
+            int addonepar = Add(10);
+            Console.WriteLine(addonepar);
+            Console.WriteLine("Add method with 2 params");
+            int twoparam = Add(10, 20);
+            Console.WriteLine(twoparam);
+            Console.WriteLine("Add method with 2 params in one is float");
+            int floatparam = Add(10, 20.5F);
+            Console.WriteLine(floatparam);
+       
+
+
+        }
+
+        static int Substrct(int a , int b)
+        {
+            return a - b;
+        }
+
+        static int Multply(int a,int b)
+        {
+            int result=a* b;
+            return result;
+        }
+
+
+        static void PrintName()
+        {
+            Console.WriteLine("My name is Abhinav ");
+        }
+
+        static void PrintGenName(string str,int age)
+        {
+            Console.WriteLine("My Name is "+str );
+            Console.WriteLine("my age is "+age);
+        }
+
+        static void VoidMethod()
+        {
+            Console.WriteLine("Hellow Again");
+            Console.WriteLine("Hellow from Void Method");
+            Console.WriteLine("Hellow again");
+        }
+        static void VoidMethodWithParams(int a, string name)
+        {
+            for(int i=0;i<a;i++)
+            {
+                Console.WriteLine(name);
+            }
+        }
+        static int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        static int Add(int a )
+        {
+            return (int)(a + 10);
+        }
+        static int Add(int a, float b)
+        {
+            return (int)( a + b);
+        }
+
+    }
+}
---
+++ IntroductionToCSharp/Functions-TypesOfArguements/Functions-Arguements.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Functions_TypesOfArguements
+{
+    public class Functions_Arguements
+    {
+       public  static void Main()
+        {
+            int a = 10;
+            int b = 20;
+            Console.WriteLine("a and b before swap "+a+" "+b);
+            Swap(a , b);
+            Console.WriteLine("a and b after swap " + a + " " + b);
+            Console.WriteLine("------------------------------------");
+            Console.WriteLine("a and b before swap " + a + " " + b);
+            SwapRef(ref a,ref b);
+            Console.WriteLine("a and b after swap " + a + " " + b);
+            Console.WriteLine("------------------------------------");
+            Console.WriteLine("a and b before swap " + a + " " + b);
+            int refvar = 10; int outvar = 10;
+            MethOut(ref refvar, out outvar);
+            Console.WriteLine(outvar + " " + refvar);
+            
+            ParamsMethod(1, 2, 3, 4, 5, 6);
+            Console.WriteLine("----------------Named and Optional Parameters-------------");
+            Console.WriteLine(  OptionalParamMethod(a));
+            Console.WriteLine(OptionalParamMethod(20,30));
+            NamedParamMethod(b: 20, a: 10);
+        }
+       
+       static void Swap(int a ,int b)
+       {
+           int temp;
+           temp = a;
+           a = b;
+           b = temp;
+       }
+       static void SwapRef(ref int a, ref int b)
+       {
+           int temp;
+           temp = a;
+           a = b;
+           b = temp;
+       }
+       static void MethOut(ref int refvar, out int outvar)
+       {
+           outvar = 20;
+           refvar = 20;
+           
+       }
+        static void ParamsMethod(params int[]arr)
+        {
+            foreach(var element in arr)
+            {
+                Console.WriteLine(element);
+            }
+        }
+        static int OptionalParamMethod(int a, int b=10)
+        {
+            return a + b;
+        }
+        static void NamedParamMethod(int a,int b)
+        {
+            Console.WriteLine(" a is "+a);
+            Console.WriteLine("b is " + b);
+        }
+    }
+}
