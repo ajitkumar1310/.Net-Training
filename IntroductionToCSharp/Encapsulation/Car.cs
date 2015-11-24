3cb11cfc5cf2884826259d9ad1dc4661b1cacb9b
S. Ajit Kumar <ajitkumar1310@hotmail.com>
2015-11-24 11:41:08 +0530

    Encapsulation changes

---
+++ IntroductionToCSharp/Encapsulation/Car.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsulation
+{
+    public class Car
+    {
+        //data
+        public string brandname;
+        public int speed;
+        //behavior
+        public void DisplaySpeed()
+        {
+            Console.WriteLine(brandname+" car is going at a speed of "+speed+" MPH");
+        }
+        public void Accelerate()
+        {
+            speed = speed + 10;
+        }
+    }
+    public class   ModifiedCar
+    {
+        //data
+        private string brandname;
+        private int speed;
+        //properties
+        public string _BrandName
+        {
+            get
+            {
+                return brandname;
+            }
+            set
+            {
+                brandname = value;
+            }
+        }
+
+        public int _Speed
+        {
+            get
+            {
+                return speed;
+            }
+
+            set
+            {
+                if (value > 200)
+                    Console.WriteLine("speed cannot be > 200 MPH");
+                else
+                    speed = value;
+            }
+        }
+        //behavior
+        public void DisplaySpeed()
+        {
+            Console.WriteLine(brandname + " car is going at a speed of " + speed + " MPH");
+        }
+        public void Accelerate()
+        {
+            speed = speed + 10;
+        }
+    }
+}
--- IntroductionToCSharp/Encapsulation/Program.cs
+++ IntroductionToCSharp/Encapsulation/Program.cs
@@ -17,24 +17,30 @@ namespace Encapsulation
             c.DisplaySpeed();//0
             c.Accelerate();
             c.DisplaySpeed();//10
-            c._BrandName = "BMW";
-            c.Speed = 150;
-            c.Speed = 500;
-           
-
-            //c.DisplaySpeed();//10
-            //c.Accelerate();
-            //c.DisplaySpeed();//20
-            //c.speed = 50;
-            //c.DisplaySpeed();//
-            //c.Accelerate();
-            //c.DisplaySpeed();
+            c.brandname = "BMW";
+            c.speed = 150;
 
 
+
+            c.DisplaySpeed();//10
+            c.Accelerate();
+            c.DisplaySpeed();//20
+            c.speed = 50;
+            c.DisplaySpeed();//
+            c.Accelerate();
+            c.DisplaySpeed();
+
+            ModifiedCar mc = new ModifiedCar();
+            mc._Speed = 500;
+            mc._Speed = 150;
+            mc._BrandName = "AUDI";
+            mc.DisplaySpeed();
+            mc.Accelerate();
+            mc.DisplaySpeed();
             //c.speed = 500;
             //c.DisplaySpeed();
 
-            
+
         }
     }
   
