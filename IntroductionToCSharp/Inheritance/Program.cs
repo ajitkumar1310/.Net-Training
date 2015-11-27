006c85e7653723226866c7985361b839657e5adf
ajitkumar1310 <ajitkumar1310@gmail.com>
2015-11-27 23:56:46 +0530

    Inheritance

---
+++ IntroductionToCSharp/Inheritance/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Shape S = new Shape();
+            
+            Rectangle R = new Rectangle();
+            Triangle T = new Triangle();
+            
+            R.CalArea();
+         
+        }
+    }
+}
