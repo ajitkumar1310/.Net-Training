b6d112e9d0d3c660c2a8d17b43b051b9b226d251
ajitkumar1310 <ajitkumar1310@gmail.com>
2015-11-28 22:56:57 +0530

    Inheritance , class diagrams

--- IntroductionToCSharp/Inheritance/Program.cs
+++ IntroductionToCSharp/Inheritance/Program.cs
@@ -10,12 +10,21 @@ namespace Inheritance
     {
         static void Main(string[] args)
         {
-            Shape S = new Shape();
+        //    Shape S = new Shape();
             
-            Rectangle R = new Rectangle();
-            Triangle T = new Triangle();
+        //    Rectangle R = new Rectangle();
+        //    Triangle T = new Triangle();
             
-            R.CalArea();
+        //    R.CalArea();
+
+            Manager m = new Manager();
+            //Manager m2 = new Manager("Ajit", 25, 001, 10000F);
+            //m2.DisplayStats();
+            string str1 = "Ajit";
+            string str2 = "Abhinav";
+            Console.WriteLine("Name: {1}", str1, str2);
+
+
          
         }
     }
---
+++ IntroductionToCSharp/MiscTypes/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiscTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MiscTyp m = new MiscTyp();
+            //const var cannnot be assigned and constvar is implicitly static.
+            //MiscTyp.consvar = 200;
+            //readvar cannot be assigned and readvar is not implicitly static
+            //m.readvar = 200;
+            PartialClass p = new PartialClass();
+            
+        }
+    }
+    class MiscTyp
+    {
+        public const int consvar = 100;//by default static
+        public readonly int readvar = 200;//by default not static instead it can be assigne in the constructor but not in other method
+
+        public MiscTyp()
+        {
+           // consvar = 200;error
+            readvar = 300;
+        }
+    }
+}
