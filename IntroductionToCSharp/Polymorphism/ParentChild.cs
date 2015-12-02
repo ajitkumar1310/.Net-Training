1476b34c4fdc6c2c0c77831120888c04c5e99b74
ajitkumar1310 <ajitkumar1310@gmail.com>
2015-12-02 09:53:53 +0530

    Polymorphism code

---
+++ IntroductionToCSharp/Polymorphism/ParentChild.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorphism
+{
+    class Parent
+    {
+        public void parentmethod()
+        {
+            Console.WriteLine("Am in parent method");
+        }
+        public  void CommonMethod()
+        {
+            Console.WriteLine("Am in parent common method");
+        }
+    }
+    class Child:Parent
+    {
+        public void childmethod()
+        {
+            Console.WriteLine("Am in child method");
+        }
+        public new void CommonMethod()
+        {
+            Console.WriteLine("Am in child common method");
+        }
+
+    }
+}
---
+++ IntroductionToCSharp/Polymorphism/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorphism
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Employee e=new Employee("Abhinav", 29, 101, 1000F);
+            //e.GiveBonus();
+            //Manager m = new Manager("Ajit", 29, 100, 1000F);
+            //m.GiveBonus();
+
+            //CEO c = new CEO("Kumar", 29, 100, 1000F);
+            //c.GiveBonus();
+            //DerivedCEO dc = new DerivedCEO("Avinash", 29, 100, 1000F);
+            //dc.GiveBonus();
+
+         //   Employee e1;
+         //   e1 = new Employee();
+         //   Manager m1;
+         //   m1 = new Manager();
+         //   e1 = new Manager();
+
+
+         ////   m1 = new Employee();
+
+         //   Console.WriteLine("Enter the object to be created Manager or Employee");
+         //   string str = Console.ReadLine();
+         //   if (str == "manager")
+         //   {
+         //       e1 = new Manager();
+         //   }
+         //   else if (str == "employee")
+         //   {
+         //       e1 = new Employee();
+         //   }
+         //   m1.SomeMethod();
+
+            Parent p = new Parent();
+            Child c = new Child();
+           /// Parent p = new Child();
+           // p.parentmethod();
+           // p.CommonMethod();
+           // c.childmethod();
+            c.CommonMethod();
+            Parent p2 = new Child();
+            p2.CommonMethod();
+        }
+    }
+}
